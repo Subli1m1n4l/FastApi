91b9ef1bf8af22048190840cd58a9e9497e10a56
Johan Espejo <johanm-espejom@outlook.com>
2023-08-01 20:50:06 -0500

    Se agregan controladores de movies

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI
-
+from fastapi.responses import HTMLResponse
+import data.infoMovies as im
 app= FastAPI()
 
 app.title=" Mi aplicacion con fastapi"
 app.version="1.0.0"
 
+
+
 @app.get('/',tags=['Home'])
 def message():
-    return 'Hola Mundo'
+    return HTMLResponse('<h1>Hola Mundo </h1>')
+
+
+@app.get('/movies',tags=['movies'])
+def Movies():
+    return im.movies
+
+
+@app.get('/movies/{id}',tags=['movies'])
+def get_movie(id:int):
+    return [item for item in im.movies if item["id"]==id]
