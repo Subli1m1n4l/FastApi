bb09a2ca457e77b4090d6e3d08f79566e37dbda3
Johan Espejo <johanm-espejom@outlook.com>
2023-08-04 22:05:53 -0500

    Se validan credenciales a partir de una lista de usuarios

--- Security/identity.py
+++ Security/identity.py
@@ -1,22 +1,21 @@
 import models.identity.User as us
 import Security.Token.jwt_manager as jwt
 
-userAdmin='johan.ageof@gmail.com'
-passwordAdmin='12345'
+
 
 identities:dict=[
     {
-        userAdmin:'johan.ageof@gmail.com',
-        passwordAdmin:'12345'
+        'userAdmin':'johan.ageof@gmail.com',
+        'passwordAdmin':'12345'
     },
     {
-        userAdmin:'pruebas@gmail.com',
-        passwordAdmin:'aaa12546'
+        'userAdmin':'pruebas@gmail.com',
+        'passwordAdmin':'aaa12546'
     }
 ]
 def valid_user(user:us.User) -> bool:
-    identity=[item for item in identities if item["userAdmin"]==user.email and item["passwordAdmin"]== user.password]
-    if identity.count > 0:
+    identity:list=[item for item in identities if item["userAdmin"]==user.email and item["passwordAdmin"]== user.password]
+    if len(identity) > 0:
         return True
     else:
         return False
